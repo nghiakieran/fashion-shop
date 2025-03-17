222e59dbbaf5f4ac6c7a9d0bae698a1ca8f95901
nghiakieran <lechinghia202@gmail.com>
2025-03-17 08:46:49 +0700

    set up context api, build latest products

---
+++ src/context/ShopContextProvider.jsx
@@ -0,0 +1,20 @@
+import { ShopContext } from "./ShopContext";
+
+import { products } from "../assets/frontend_assets/assets";
+
+const ShopContextProvider = ({ children }) => {
+
+  const currency = '$'
+  const delivery_fee = 10
+  const value = {
+    products, currency, delivery_fee
+  }
+
+  return (
+    <ShopContext.Provider value={value}>
+      {children}
+    </ShopContext.Provider>
+  )
+}
+
+export default ShopContextProvider

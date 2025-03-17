31004f02e21921ab039c8bdc2097a8690d6f1e58
nghiakieran <lechinghia202@gmail.com>
2025-03-17 09:10:24 +0700

    build best sellers

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -4,30 +4,28 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
 
-const LatestCollection = () => {
+const BestSellers = () => {
 
   const { products } = useContext(ShopContext)
-  const [latestProducts, setLatestProducts] = useState([])
-  
+  const [bestSellers, setBestSellers] = useState([])
+
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10))
+    const bestProducts = products.filter(p => p.bestseller)
+    setBestSellers(bestProducts.slice(0, 5))
   }, [products])
 
   return (
     <div className='my-10'>
-      <div className='text-center py-8 text-2xl sm:text-3xl '>
-        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+      <div className='text-center py-8 text-2xl sm:text-3xl'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
         <p className='w-3/4 mx-auto text-xs sm:text-sm md:text-base text-gray-600'>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
         </p>
       </div>
-      {/* Rendering products */}
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
         {
-          latestProducts.map((product) => (
-            <ProductItem key={product._id} id={product._id} name={product.name} price={product.price} 
-              image={product.image} 
-            />
+          bestSellers.map(p => (
+            <ProductItem key={p._id} id={p._id} name={p.name} image={p.image} price={p.price} />
           ))
         }
       </div>
@@ -35,4 +33,4 @@ const LatestCollection = () => {
   )
 }
 
-export default LatestCollection
+export default BestSellers
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import BestSellers from '../components/BestSellers'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSellers />
     </div>
   )
 }
